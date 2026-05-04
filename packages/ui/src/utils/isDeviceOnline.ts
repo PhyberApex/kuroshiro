9134e067ccb7b1234a7ce9aead10de3d39206878
PhyberApex <phyberapex@phyberapex.de>
2026-05-04 09:03:43 +0200

    chore(deps): update dependency @antfu/eslint-config to v8

--- packages/ui/src/utils/isDeviceOnline.ts
+++ packages/ui/src/utils/isDeviceOnline.ts
@@ -3,7 +3,7 @@ import type { Device } from '../types'
 export function isDeviceOnline(device: Device) {
   if (!device.refreshRate)
     return false
-  const timeDifferenceSeconds = (new Date().getTime() - new Date(device.lastSeen).getTime()) / 1000
+  const timeDifferenceSeconds = (Date.now() - new Date(device.lastSeen).getTime()) / 1000
 
   return timeDifferenceSeconds <= device.refreshRate
 }
